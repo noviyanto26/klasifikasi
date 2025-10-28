fe27ff1d56f8fd81f762830d653c5c623c02d5c7
Noviyanto <noviyanto26@gmail.com>
2025-10-28 07:20:27 +0700

    Update 9f_agentic_with_cache.py

--- 9f_agentic_with_cache.py
+++ 9f_agentic_with_cache.py
@@ -832,7 +832,7 @@ if df_hasil is not None:
                     "cache_hasil.json",
                     key=PG + "dl_json_cache",
                     help="Simpan file ini! Upload file ini di sesi berikutnya untuk melanjutkan progres.",
-                    use_container_width=Trove
+                    use_container_width=True
                 )
         # --- END: MODIFIKASI 5 ---
 
